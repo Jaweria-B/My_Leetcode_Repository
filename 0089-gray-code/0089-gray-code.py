a881926eeb572500917ec2f100c617733ba1159c
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-08-09 18:15:52 +0500

    Time: 90 ms (91.05%), Space: 24 MB (20.41%) - LeetHub

--- 0089-gray-code/0089-gray-code.py
+++ 0089-gray-code/0089-gray-code.py
@@ -1,27 +1,11 @@
 class Solution:
     def grayCode(self, n: int) -> List[int]:
-        def calculate(n):
-            
-            if n == 1:
-                return ["0", "1"]
-            
-            temp = calculate(n-1)
-            ans = []
-            
-            for i in range(len(temp)):
-                ans.append("0" + temp[i])
-            
-            for i in range(len(temp)-1, -1, -1):
-                ans.append("1" + temp[i])
-                
-            return ans
-            
-        combinations = calculate(n)
-        
-        ans = []
-        for comb in combinations:
-            ans.append(int(comb, 2))
-        
-        return ans
+        codes = [0]
+        offset = 1
+        for i in range(n):
+            for j in range(len(codes)-1, -1, -1):
+                codes.append(codes[j]+offset)
+            offset = offset << 1
+        return codes
     
         
