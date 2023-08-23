91b2e1f4c07addb3a25e17925e167ad44284e405
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-08-23 21:12:05 +0500

    Time: 37 ms (90.18%), Space: 16.3 MB (78.11%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,26 +1,30 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        ans = []
+        char_counts = Counter(s)
+        max_count, letter = 0 , ''
         
-        pq = [(-count, char) for char, count in Counter(s).items()]
+        for char, count in char_counts.items():
+            if count > max_count:
+                max_count = count
+                letter = char
         
-        heapify(pq)
+        if max_count > (len(s) + 1) //2:
+            return ""
         
-        while pq:
-            count_first, char_first = heappop(pq)
+        ans = [''] * len(s)
+        index = 0
+        
+        while char_counts[letter] != 0:
+            ans[index] = letter
+            index += 2
+            char_counts[letter] -= 1
             
-            if not ans or char_first != ans[-1]:
-                ans.append(char_first)
-                
-                if count_first +1 != 0:
-                    heappush(pq, (count_first +1, char_first))
-                    
-            else:
-                if not pq: return ''
-                count_second, char_second = heappop(pq)
-                ans.append(char_second)
-                if count_second + 1 != 0:
-                    heappush(pq, (count_second + 1, char_second))
-                heappush(pq, (count_first, char_first))
+        for char, count in char_counts.items():
+            while count > 0:
+                if index >= len(s):
+                    index = 1
+                ans[index] = char
+                index += 2
+                count -= 1
         
         return ''.join(ans)
