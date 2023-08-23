6c592817fa2f7d84ffb3755d2090c30f2a9087cc
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-08-23 21:23:20 +0500

    Time: 44 ms (64.10%), Space: 16.4 MB (46.15%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -1,30 +1,38 @@
 class Solution:
     def reorganizeString(self, s: str) -> str:
-        char_counts = Counter(s)
-        max_count, letter = 0 , ''
+#       making a dictionary
+        dic = {}
+#       storing the count of each character in the dictionary 
+        for i in s:
+            if i not in dic:
+                dic[i] = 1
+            else:
+                dic[i] += 1
+#       making a list so we can store all the characters along with their count 
+        ch_cnt = []
+#       placing the value variable first because we're gonna sort it
+        for k, val in dic.items():
+            ch_cnt.append((val, k))
+#       sorting the list to get the most frequent character at left
+        ch_cnt.sort()
         
-        for char, count in char_counts.items():
-            if count > max_count:
-                max_count = count
-                letter = char
-        
-        if max_count > (len(s) + 1) //2:
+#       making a list instead of a string variable as we're gonna place characters seperately using list as a grid
+        ans = [0] * len(s)
+#       if even the last character in list has more frequency than half of the list than it won't be possible to make a combination
+        if ch_cnt[-1][0] > (len(s)+1)//2:
             return ""
-        
-        ans = [''] * len(s)
-        index = 0
-        
-        while char_counts[letter] != 0:
-            ans[index] = letter
-            index += 2
-            char_counts[letter] -= 1
+#       placing the designated popped character with a space of 1 index on the grid 
+        i = 0
+        while ch_cnt:
+            cnt, ch = ch_cnt.pop()
             
-        for char, count in char_counts.items():
-            while count > 0:
-                if index >= len(s):
-                    index = 1
-                ans[index] = char
-                index += 2
-                count -= 1
-        
-        return ''.join(ans)
+            while cnt != 0:
+                ans[i] = ch
+                cnt -= 1
+                i += 2
+                if i >= len(s):
+                    i = 1
+#       every index on ans array will be filled as its length is same as that of string
+
+#       returning the answer as a string not as list/grid
+        return "".join(ans)
