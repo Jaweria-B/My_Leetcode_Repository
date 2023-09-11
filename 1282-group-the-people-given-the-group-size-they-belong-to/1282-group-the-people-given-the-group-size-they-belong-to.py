e71d31047009af603bf2ebf6e7f46b9b87068620
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-09-11 20:35:11 +0500

    Time: 69 ms (84.71%), Space: 16.2 MB (99.33%) - LeetHub

---
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.py
@@ -0,0 +1,11 @@
+class Solution:
+    def groupThePeople(self, groupSizes: List[int]) -> List[List[int]]:
+        groups = defaultdict(list)
+        ans = []
+        
+        for pid, group_size in enumerate(groupSizes):
+            groups[group_size].append(pid)
+            if len(groups[group_size]) == group_size:
+                ans.append(groups.pop(group_size))
+        
+        return ans
