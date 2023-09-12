709a031a42ae0ca381e5dd888c3c25d1f5f3669e
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-09-12 20:15:14 +0500

    Time: 136 ms (61.54%), Space: 17.2 MB (62.10%) - LeetHub

---
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minDeletions(self, s: str) -> int:
+        
+        cnt = Counter(s)
+        deletions = 0
+        used_frequencies = set()
+        
+        for char, freq in cnt.items():
+            while freq > 0 and freq in used_frequencies:
+                freq -= 1
+                deletions += 1
+            used_frequencies.add(freq)
+        return deletions
