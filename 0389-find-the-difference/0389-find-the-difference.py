a84d1e7a59c30d0f5f5bbb1f97321d198c00a6cc
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-09-25 21:30:14 +0500

    Time: 28 ms (99.02%), Space: 16.3 MB (43.09%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,6 +1,6 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        c = 0
-        for cs in s: c ^= ord(cs) #ord is ASCII value
-        for ct in t: c ^= ord(ct)
-        return chr(c) #chr = convert ASCII into character
+        result = 0
+        for char in s + t:
+            result ^= ord(char)
+        return chr(result)
