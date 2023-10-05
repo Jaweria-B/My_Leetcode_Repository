ca5e4e36977321a2700e2ca5103082653b2230ff
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-10-05 21:55:35 +0500

    Time: 108 ms (72.53%), Space: 17.7 MB (84.36%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,22 +1,48 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
-        if not nums:
-            return []
-        
-        candidate1, candidate2, count1, count2 = 0, 1, 0, 0
-        
+        # Counters for the potential majority elements
+        count1 = count2 = 0     
+        # Potential majority element candidates
+        candidate1 = candidate2 = 0
+
+        # First pass to find potential majority elements.
         for num in nums:
-            if num == candidate1:
+            # If count1 is 0 and the current number is not equal to candidate2, update candidate1.
+            if count1 == 0 and num != candidate2:
+                count1 = 1
+                candidate1 = num
+
+            # If count2 is 0 and the current number is not equal to candidate1, update candidate2.
+            elif count2 == 0 and num != candidate1:
+                count2 = 1
+                candidate2 = num
+            
+            # Update counts for candidate1 and candidate2.
+            elif candidate1 == num:
                 count1 += 1
-            elif num == candidate2:
+            elif candidate2 == num:
                 count2 += 1
-            elif count1 == 0:
-                candidate1, count1 = num, 1
-            elif count2 == 0:
-                candidate2, count2 = num, 1
+
+            # If the current number is different from both candidates, decrement their counts.
             else:
                 count1 -= 1
                 count2 -= 1
-            
-        
-        return [num for num in (candidate1, candidate2) if nums.count(num) > len(nums) // 3]
+
+        result = []
+        threshold = len(nums) // 3  # Threshold for majority element
+
+        # Second pass to count occurrences of the potential majority elements.
+        count1 = count2 = 0
+        for num in nums:
+            if candidate1 == num:
+                count1 += 1
+            elif candidate2 == num:
+                count2 += 1
+
+        # Check if the counts of potential majority elements are greater than n/3 and add them to the result.
+        if count1 > threshold:
+            result.append(candidate1)
+        if count2 > threshold:
+            result.append(candidate2)
+
+        return result
