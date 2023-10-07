0902bc20ea41620cc160edd7332a3180e260c899
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-10-07 11:57:02 +0500

    Time: 101 ms (63.05%), Space: 16.1 MB (98.88%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,9 +1,13 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        res = []
+        res = set()
         for i in range(9):
             for j in range(9):
                 element = board[i][j]
                 if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        return len(res) == len(set(res))
+                    if (i, element) in res or (element, j) in res or (i // 3, j // 3, element) in res :
+                        return False
+                    res.add((i, element))
+                    res.add((element, j))
+                    res.add((i // 3, j // 3, element))
+        return True
