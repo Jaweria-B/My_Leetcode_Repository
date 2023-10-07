44bc1a1b91b076d4432e6679d26a41ca6a662c3f
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-10-07 11:56:02 +0500

    Time: 90 ms (97.29%), Space: 16.1 MB (90.73%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        res = []
+        for i in range(9):
+            for j in range(9):
+                element = board[i][j]
+                if element != '.':
+                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
+        return len(res) == len(set(res))
