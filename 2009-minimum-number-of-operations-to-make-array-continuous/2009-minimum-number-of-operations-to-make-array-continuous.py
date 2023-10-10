ab04e3567ef8798187321b3e277aae0c72c78dff
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-10-10 17:39:49 +0500

    Time: 719 ms (48.68%), Space: 35.1 MB (87.50%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(nums)
         ans = n
         new_nums = sorted(set(nums))
+        j = 0
         
-        for i in range(len(new_nums)):
-            left = new_nums[i]
-            right = left + n - 1
-            j = bisect_right(new_nums, right)
-            count = j-i
+        for i in range(len(nums)):
+            while j < len(new_nums) and new_nums[j] < new_nums[i] + n:
+                j += 1
+            count = j - i
             ans = min(ans, n - count)
         
         return ans
