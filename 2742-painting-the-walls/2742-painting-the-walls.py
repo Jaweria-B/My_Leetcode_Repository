b4e9a6e1058b44f5f1563771be5ef32ec79fab85
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-10-14 14:22:54 +0500

    Time: 1275 ms (58.15%), Space: 26 MB (51.85%) - LeetHub

--- 2742-painting-the-walls/2742-painting-the-walls.py
+++ 2742-painting-the-walls/2742-painting-the-walls.py
@@ -1,17 +1,17 @@
 class Solution:
     def paintWalls(self, cost: List[int], time: List[int]) -> int:
         
-        @cache
-        
-        def dp(i, remain):
-            if remain <= 0:
-                return 0
-            if i == n:
-                return inf
-            
-            paint = cost[i] + dp(i+1, remain - 1 - time[i])
-            dont_paint = dp(i+1, remain)
-            return min(paint, dont_paint)
-        
         n = len(cost)
-        return dp(0, n)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        
+        for i in range(1, n + 1):
+            dp[n][i] = inf
+            
+        for i in range(n-1, -1, -1):
+            for remain in range(1, n + 1):
+                paint = cost[i] + dp[i + 1][max(0, remain - 1 - time[i])]
+                dont_paint = dp[i + 1][remain]
+                dp[i][remain] = min(paint, dont_paint)
+                
+        
+        return dp[0][n]
