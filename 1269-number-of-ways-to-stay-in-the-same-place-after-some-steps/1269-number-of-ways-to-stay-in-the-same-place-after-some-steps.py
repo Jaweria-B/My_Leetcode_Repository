3bb6f43cb945b385feb750842059cd014e9a1891
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-10-15 11:57:07 +0500

    Time: 246 ms (80.00%), Space: 16.4 MB (86.11%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -2,18 +2,24 @@ class Solution:
     def numWays(self, steps: int, arrLen: int) -> int:
         MOD = 10 ** 9 + 7
         arrLen = min(arrLen, steps)
-        
-        dp = [[0] * (steps + 1) for _ in range(arrLen)]
-        dp[0][0] = 1
+        dp = [0] * (arrLen)
+        prevDp = [0] * (arrLen)
+        prevDp[0] = 1
         
         for remain in range(1, steps + 1):
+            dp = [0] * (arrLen)
+            
             for curr in range(arrLen - 1, -1, -1):
-                ans = dp[curr][remain - 1]
+                ans = prevDp[curr]
                 
-                ans = (ans + dp[curr - 1][remain - 1]) % MOD if curr > 0 else ans
+                if curr > 0:
+                    ans = (ans + prevDp[curr - 1]) % MOD
                 
-                ans = (ans + dp[curr + 1][remain - 1]) % MOD if curr < arrLen - 1 else ans
+                if curr < arrLen - 1:
+                    ans = (ans + prevDp[curr + 1]) % MOD
                 
-                dp[curr][remain] = ans
+                dp[curr] = ans
                 
-        return dp[0][steps]
+            prevDp = dp
+        
+        return dp[0]
