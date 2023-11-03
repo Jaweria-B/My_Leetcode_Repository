747a4e141c2b6b3a6688e1d62215860e37e13dd2
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-11-03 14:28:29 +0500

    Time: 38 ms (69.50%), Space: 16.1 MB (76.57%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.py
@@ -1,16 +1,20 @@
 class Solution:
     def buildArray(self, target: List[int], n: int) -> List[str]:
         
-        ans = []
-        i = 0
+        operations = []
+        currNum = 1
+        index = 0
         
-        for num in target:
-            while i < num - 1:
-                ans.append("Push")
-                ans.append("Pop")
-                i += 1
-                
-            ans.append("Push")
-            i += 1
-        
-        return ans
+        while currNum <= n and index < len(target):
+            num = target[index]
+            
+            if num == currNum:
+                operations.append("Push")
+                currNum += 1
+                index += 1
+            else:
+                operations.append("Push")
+                operations.append("Pop")
+                currNum += 1
+            
+        return operations
