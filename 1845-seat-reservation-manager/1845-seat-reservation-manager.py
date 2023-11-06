fe770f5661305e364338558a7a8a804ef14f7889
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-11-06 20:01:52 +0500

    Time: 386 ms (93.97%), Space: 42.6 MB (92.09%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.py
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.py
@@ -1,10 +1,17 @@
 class SeatManager:
 
     def __init__(self, n: int):
-        self.available_seats = [i for i in range(1, n+1)]
+        self.marker = 1
+        
+        self.available_seats = []
 
     def reserve(self) -> int:
-        seat_number = heapq.heappop(self.available_seats)
+        if self.available_seats:
+            seat_number = heapq.heappop(self.available_seats)
+            return seat_number
+        
+        seat_number = self.marker 
+        self.marker += 1
         return seat_number
 
     def unreserve(self, seatNumber: int) -> None:
