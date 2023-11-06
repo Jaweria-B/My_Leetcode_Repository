63b7937691f819b9172143ed1f3b33e84ca2d308
Jaweria-B <118465170+Jaweria-B@users.noreply.github.com>
2023-11-06 19:19:12 +0500

    Time: 222 ms (59.64%), Space: 26.2 MB (18.80%) - LeetHub

--- 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
+++ 1128-number-of-equivalent-domino-pairs/1128-number-of-equivalent-domino-pairs.py
@@ -1,14 +1,4 @@
 class Solution:
     def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
         
-        count = {}
-        res = 0
-        for d in dominoes:
-            ans = min(d[0], d[1]) * 10 + max(d[0], d[1])
-            if ans not in count:
-                count[ans] = 1
-            else:
-                res += count[ans]
-                count[ans] += 1
-                
-        return res
+        return sum( v * (v - 1) // 2 for v in collections.Counter( tuple( sorted(x) ) for x in dominoes ).values() )
