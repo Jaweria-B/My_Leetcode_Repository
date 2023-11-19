1a7ffb7f83f873fa2ade6e3f68d5233cc4d244db
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2023-11-19 16:13:58 +0500

    Time: 868 ms (31.44%), Space: 24 MB (45.88%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -1,16 +1,9 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        n = len(nums)
-        freq = [0] * 50001
-        
-        for i in nums:
-            freq[i] += 1
-        
-        res, operations = 0, 0
-        
-        for i in range(50000, 0, -1):
-            if freq[i] > 0:
-                operations += freq[i]
-                res += operations - freq[i]
-                
-        return res
+        n=len(nums)
+        nums.sort(reverse=True)
+        ans=0
+        for i in range(n-1):
+            if nums[i]>nums[i+1]:
+                ans+=i+1
+        return ans 
