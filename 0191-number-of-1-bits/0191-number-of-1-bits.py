528b65bfdb67b08455ce597b749b5a3177f68e22
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2023-11-29 20:16:33 +0500

    Time: 37 ms (70.55%), Space: 16.3 MB (5.24%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -3,8 +3,7 @@ class Solution:
         count = 0
         
         while n != 0:
-            if n & 1:
-                count += 1
+            count += n & 1
             n >>= 1
         
         return count
