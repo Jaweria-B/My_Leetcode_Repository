11dc0f66d1e4e271168dee6674f053c969766ef0
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2023-12-14 14:04:01 +0500

    Time: 1322 ms (93.57%), Space: 43.5 MB (90.71%) - LeetHub

--- 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
+++ 2482-difference-between-ones-and-zeros-in-row-and-column/2482-difference-between-ones-and-zeros-in-row-and-column.py
@@ -1,30 +1,12 @@
 class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
-        onesRow = []
-        zerosRow = []
-        onesCol = []
-        zerosCol = []
-        
-        for r in grid:
-            count_1s = r.count(1)
-            count_0s = len(r) - count_1s
-            onesRow.append(count_1s)
-            zerosRow.append(count_0s)
-                    
-        for i in range(len(grid[0])):
-            count_1s = 0
-            for j in range(len(grid)):
-                if grid[j][i] == 1:
-                    count_1s += 1
+        def summation(nums) : 
+            return 2 * sum(nums) - len(nums)
+        m, n = len(grid), len(grid[0])
             
-            count_0s = len(grid) - count_1s
-            onesCol.append(count_1s)
-            zerosCol.append(count_0s)
+        rows = list(map(summation, grid))
+        cols = list(map(summation, zip(*grid)))
         
-        ans = [[0]*len(grid[0]) for _ in range(len(grid))]
-        
-        for i in range(len(ans)):
-            for j in range(len(ans[0])):
-                ans[i][j] = onesRow[i] + onesCol[j] - zerosRow[i] - zerosCol[j]
-
-        return ans
+        for i,j in product(range(m), range(n)):
+            grid[i][j] = rows[i] + cols[j]
+        return grid
