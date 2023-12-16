cfba410320ba265cd296705d9060270b20e10346
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2023-12-16 15:30:39 +0500

    Time: 45 ms (90.22%), Space: 17.5 MB (19.13%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,14 +1,9 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_list = [0] * 26
-        for ch in s:
-            s_list[ord(ch) - ord('a')] += 1
-            
-        t_list = [0] * 26
-        for ch in t:            
-            t_list[ord(ch) - ord('a')] += 1
-            
-
+        a = list(s)
+        b = list(t)
+       
+        a.sort()
+        b.sort()
         
-        return s_list == t_list
-                
+        return a == b
