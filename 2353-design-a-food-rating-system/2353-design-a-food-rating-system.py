b73597a36395753faaf222433562fd87b2f8e317
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2023-12-17 18:52:55 +0500

    Time: 964 ms (28.57%), Space: 52.2 MB (11.91%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -0,0 +1,43 @@
+from sortedcontainers import SortedSet
+
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+       # Map food with its rating.
+        self.food_rating_map = {}
+        # Map food with cuisine it belongs to.
+        self.food_cuisine_map = {}
+
+        # Store all food of a cuisine in a set (to sort them on ratings/name)
+        # Set element -> Tuple: (-1 * food_rating, food_name)
+        self.cuisine_food_map = defaultdict(SortedSet)
+
+        for i in range(len(foods)):
+            # Store 'rating' and 'cuisine' of the current 'food' in 'food_rating_map' and 'food_cuisine_map' maps.
+            self.food_rating_map[foods[i]] = ratings[i]
+            self.food_cuisine_map[foods[i]] = cuisines[i]
+            # Insert the '(-1 * rating, name)' element in the current cuisine's set.
+            self.cuisine_food_map[cuisines[i]].add((-ratings[i], foods[i]))
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        # Fetch cuisine name for food.
+        cuisine_name = self.food_cuisine_map[food]
+
+        # Find and delete the element from the respective cuisine's set.
+        old_element = (-self.food_rating_map[food], food)
+        self.cuisine_food_map[cuisine_name].remove(old_element)
+
+        # Update food's rating in 'food_rating' map.
+        self.food_rating_map[food] = newRating
+        # Insert the '(-1 * new rating, name)' element in the respective cuisine's set.
+        self.cuisine_food_map[cuisine_name].add((-newRating, food))
+
+    def highestRated(self, cuisine: str) -> str:
+        highest_rated = self.cuisine_food_map[cuisine][0]
+        # Return name of the highest-rated 'food' of 'cuisine'.
+        return highest_rated[1]
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
