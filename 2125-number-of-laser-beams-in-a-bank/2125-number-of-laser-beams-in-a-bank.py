37b8aef10f06508c0ec3342976a2177614772c89
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-01-03 22:43:04 +0500

    Time: 374 ms (5.01%), Space: 19.6 MB (6.86%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.py
@@ -1,21 +1,17 @@
 class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
-        row_count = [0] * (len(bank) + 1)
-        
-        for i in range(len(bank)):
-            for j in range(len(bank[0])):
-                if bank[i][j] == '1':
-                    row_count[i] += 1
-        
+        prev_row_count = 0
         total = 0
-        
-        for r in range(len(row_count)):
-            if r == len(row_count) - 1:
+
+        for row in bank:
+            cur_row_count = self.calc(row)
+            if cur_row_count == 0:
                 continue
-            else:
-                pointer = r + 1
-                while pointer  <  len(row_count) - 1 and row_count[pointer] == 0  :
-                    pointer += 1
-                total += row_count[r] * row_count[pointer]
-        
+
+            total += cur_row_count * prev_row_count
+            prev_row_count = cur_row_count
+
         return total
+
+    def calc(self, s):
+        return sum(int(c) for c in s)
