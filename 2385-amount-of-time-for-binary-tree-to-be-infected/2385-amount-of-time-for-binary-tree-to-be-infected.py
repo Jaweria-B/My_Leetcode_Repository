298b2dec17e60c227fe201dcf8585ad1ac9ee6fe
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-01-13 15:32:27 +0500

    Time: 250 ms (94.75%), Space: 51.1 MB (96.36%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.py
@@ -5,37 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def amountOfTime(self, root: TreeNode, start: int) -> int:
-        tree_map: Dict[int, Set[int]] = {}
-        self.convert(root, 0, tree_map)
-        queue = deque([start])
-        minute = 0
-        visited = {start}
+    def __init__(self):
+        self.maxDistance = 0
+    
+    def amountOfTime(self, root, start):
+        self.traverse(root, start)
+        return self.maxDistance
+    
+    def traverse(self, root, start):
+        depth = 0
+        if root is None:
+            return depth
+        
+        leftDepth = self.traverse(root.left, start)
+        rightDepth = self.traverse(root.right, start)
 
-        while queue:
-            level_size = len(queue)
-            while level_size > 0:
-                current = queue.popleft()
-                for num in tree_map[current]:
-                    if num not in visited:
-                        visited.add(num)
-                        queue.append(num)
-                level_size -= 1
-            minute += 1
-
-        return minute - 1
-
-    def convert(self, current: TreeNode, parent: int, tree_map: Dict[int, Set[int]]):
-        if current is None:
-            return
-        if current.val not in tree_map:
-            tree_map[current.val] = set()
-        adjacent_list = tree_map[current.val]
-        if parent != 0:
-            adjacent_list.add(parent)
-        if current.left:
-            adjacent_list.add(current.left.val)
-        if current.right:
-            adjacent_list.add(current.right.val)
-        self.convert(current.left, current.val, tree_map)
-        self.convert(current.right, current.val, tree_map)
+        if root.val == start:
+            self.maxDistance = max(leftDepth, rightDepth)
+            depth = -1
+        elif leftDepth >= 0 and rightDepth >= 0:
+            depth = max(leftDepth, rightDepth) + 1
+        else:
+            distance = abs(leftDepth) + abs(rightDepth)
+            self.maxDistance = max(self.maxDistance, distance)
+            depth = min(leftDepth, rightDepth) - 1
+        
+        return depth
