88899aa8b21af8b470a6c837746562bf96529047
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-01-19 21:46:01 +0500

    Time: 109 ms (95.67%), Space: 17.7 MB (66.14%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.py
@@ -1,29 +1,13 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         N = len(matrix)
-        cache = {}
         
-        def dfs(r, c):
-            if r == N:
-                return 0
-            if c < 0 or c == N:
-                return float('inf')
-            
-            if (r, c) in cache:
-                return cache[(r, c)]
-            
-            res = matrix[r][c] + min(
-                dfs(r + 1, c - 1),
-                dfs(r + 1, c),
-                dfs(r + 1, c + 1)
-            )
-            
-            cache[(r, c)] = res
-            
-            return res
-            
-        res = float("inf")
-        for c in range(N):
-            res = min(res, dfs(0, c))
-        return res
-            
+        for r in range(1, N):
+            for c in range(N):
+                middle = matrix[r - 1][c]
+                left = matrix[r - 1][c - 1] if c > 0 else float('inf')
+                right = matrix[r - 1][c + 1] if c < N - 1 else float('inf')
+                
+                matrix[r][c] = matrix[r][c] + min(middle, left, right)
+                
+        return min(matrix[-1])
