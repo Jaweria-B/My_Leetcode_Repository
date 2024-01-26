b2ec06902a99e612863b3188ccb364ef7bdf8759
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-01-26 21:13:35 +0500

    Time: 86 ms (74.28%), Space: 19.9 MB (65.90%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.py
@@ -1,26 +1,25 @@
 class Solution:
     def findPaths(self, m: int, n: int, maxMove: int, startRow: int, startColumn: int) -> int:
         MOD = 10 ** 9 + 7
-        cache = {}
-        
+        memo = [[[-1 for _ in range(maxMove+1)] for _ in range(n)] for _ in range(m)]
         def dfs(curr_r, curr_c, moves):
-            
-            if (curr_r == m or curr_r == -1 or curr_c == n or curr_c == -1) and moves >= 0:
+            if curr_r < 0 or curr_c < 0 or curr_c >= n or curr_r >= m: 
                 return 1
             
             if moves == 0:
                 return 0
             
-            if (curr_r, curr_c, moves) in cache:
-                return cache[(curr_r, curr_c, moves)]
+            if memo[curr_r][curr_c][moves] != -1:
+                return memo[curr_r][curr_c][moves]
+    
+            bottom = dfs(curr_r+1, curr_c, moves-1)
+            top = dfs(curr_r-1, curr_c, moves-1)
+            right = dfs(curr_r, curr_c+1, moves-1)
+            left = dfs(curr_r, curr_c-1, moves-1)
             
-            bottom = dfs(curr_r + 1, curr_c, moves - 1)
-            top = dfs(curr_r - 1, curr_c, moves - 1)
-            right = dfs(curr_r, curr_c + 1, moves - 1)
-            left = dfs(curr_r, curr_c - 1, moves - 1)
+            memo[curr_r][curr_c][moves] = bottom + top + right + left
             
-            cache[(curr_r, curr_c, moves)] = (top + bottom + right + left) % MOD
-            
-            return cache[(curr_r, curr_c, moves)]
+            return memo[curr_r][curr_c][moves]
         
-        return dfs(startRow, startColumn, maxMove) % MOD
+        return dfs(startRow, startColumn, maxMove)%MOD
+        
