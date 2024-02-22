7e11f7782d29a59a912ff4e7938d09aa913bc26f
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-02-22 18:56:47 +0500

    Time: 600 ms (70.51%), Space: 21.4 MB (95.97%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,14 +1,13 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        incoming = defaultdict(int)
-        outgoing = defaultdict(int)
+        delta = defaultdict(int)
         
         for p1, p2 in trust:
-            outgoing[p1] += 1
-            incoming[p2] += 1
+            delta[p1] -= 1
+            delta[p2] += 1
         
         for i in range(1, n + 1):
-            if outgoing[i] == 0 and incoming[i] == n - 1:
+            if delta[i] == n - 1:
                 return i
             
         return -1 
