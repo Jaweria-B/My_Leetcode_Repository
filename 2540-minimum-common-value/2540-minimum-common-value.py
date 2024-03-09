f09b7324c0ddb7e478d474896330e91682cdcacb
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-03-09 20:32:43 +0500

    Time: 344 ms (75.17%), Space: 42.7 MB (21.26%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,13 +1,10 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        # Add the elements from nums1 to set1
         set1 = set(nums1)
+        set2 = set(nums2)
+        common = set1.intersection(set2)
 
-        # Search for each element of nums2 in set1
-        # Return the first common element found
-        for num in nums2:
-            if num in set1:
-                return num
-
-        # Return -1 if there are no common elements
-        return -1
+        if common:
+            return min(common)
+        else: 
+            return -1
