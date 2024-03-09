31194cb77199b575027289449859b66cbcb902cb
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-03-09 20:32:00 +0500

    Time: 357 ms (40.28%), Space: 39.7 MB (36.50%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        # Add the elements from nums1 to set1
+        set1 = set(nums1)
+
+        # Search for each element of nums2 in set1
+        # Return the first common element found
+        for num in nums2:
+            if num in set1:
+                return num
+
+        # Return -1 if there are no common elements
+        return -1
