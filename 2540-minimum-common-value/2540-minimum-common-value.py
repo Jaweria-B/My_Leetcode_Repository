bd0ada4a9e923011952bb17d848f3b6c88690b17
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-03-09 20:33:16 +0500

    Time: 353 ms (50.00%), Space: 35.6 MB (53.64%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.py
+++ 2540-minimum-common-value/2540-minimum-common-value.py
@@ -1,10 +1,18 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        set1 = set(nums1)
-        set2 = set(nums2)
-        common = set1.intersection(set2)
+        first = 0
+        second = 0
 
-        if common:
-            return min(common)
-        else: 
-            return -1
+        # Traverse through both arrays with two pointers
+        # Increment the pointer with a smaller value at that index
+        # Return the first common element found
+        while first < len(nums1) and second < len(nums2):
+            if nums1[first] < nums2[second]:
+                first += 1
+            elif nums1[first] > nums2[second]:
+                second += 1
+            else:
+                return nums1[first]
+
+        # Return -1 if there are no common elements
+        return -1
