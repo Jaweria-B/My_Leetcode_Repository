801fe383adfc3088fe4cfeeb9bb10c1b22897cab
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-04-07 02:46:52 +0500

    Time: 76 ms (62.78%), Space: 17.8 MB (98.08%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,23 +1,26 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        res = []
-        count = 0
-        
-        for c in s:
-            if c == "(":
-                res.append(c)
-                count += 1
-            elif c  == ")" and count > 0:
-                res.append(c)
-                count -= 1
-            elif c != ")":
-                res.append(c)
-        
-        filtered = []
-        
-        for c in res[::-1]:
-            if c == "(" and count > 0:
-                count -= 1
-            else: filtered.append(c)
-        
-        return "".join(filtered[::-1])
+        stack = []
+        to_remove = set()
+
+        # Identify the indices of parentheses to remove
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                else:
+                    to_remove.add(i)
+
+        # Include the remaining unmatched opening parentheses in to_remove
+        to_remove.update(stack)
+
+        # Construct the valid string without the characters in to_remove
+        result = ''
+        for i, char in enumerate(s):
+            if i not in to_remove:
+                result += char
+
+        return result
+
