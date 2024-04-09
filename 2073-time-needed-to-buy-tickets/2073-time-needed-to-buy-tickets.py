1ffbe2b6f793babd3b8feda9f4fee471d03df053
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-04-10 01:39:26 +0500

    Time: 39 ms (82.27%), Space: 16.5 MB (51.50%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        time = 0
+        
+        for i in range(len(tickets)):
+            # If the current person is before or at the desired person 'k'
+            if i <= k:
+                # Buy the minimum of tickets available at person 'k' and the current person
+                time += min(tickets[k], tickets[i])
+            else:
+                # If the current person is after 'k', buy the minimum of 
+                # (tickets available at person 'k' - 1) and the current person
+                time += min(tickets[k] - 1, tickets[i])
+        
+        return time
