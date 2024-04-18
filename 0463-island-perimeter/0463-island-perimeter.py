e44ade7d99723434c9787dc65b3e90ca27255629
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-04-19 03:24:16 +0500

    Time: 338 ms (96.69%), Space: 16.9 MB (65.88%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,23 +1,15 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        visit = set()
-        
-        def dfs(i, j):
-            if i >= len(grid) or j >= len(grid[0]) or \
-            i < 0 or j < 0 or grid[i][j] == 0:
-                return 1
-            
-            if (i, j) in visit:
-                return 0
-            
-            visit.add((i, j))
-            perim = dfs(i, j + 1)
-            perim += dfs(i + 1, j)
-            perim += dfs(i, j - 1)
-            perim += dfs(i - 1, j)
-            return perim
-        
+        perimeter = 0
+
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                if grid[i][j]:
-                    return dfs(i, j)
+                if grid[i][j] == 1:
+                    perimeter += 4
+
+                    if i > 0 and grid[i - 1][j] == 1:  # Up
+                        perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1:  # Left
+                        perimeter -= 2
+
+        return perimeter
