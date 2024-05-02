57767e10c71a35ba4a31c964d21ea66645d92636
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-05-03 01:11:47 +0500

    Time: 102 ms (86.98%), Space: 16.8 MB (23.92%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.py
@@ -2,15 +2,18 @@ class Solution:
     def findMaxK(self, nums: List[int]) -> int:
         ans = -1
 
-        # A set to store seen numbers
+        # Initialize a set to keep track of seen numbers
         seen = set()
 
         for num in nums:
             abs_num = abs(num)
 
-            # If the absolute value is greater than the current maximum and its negation is seen
-            if abs_num > ans and -num in seen:
+            # If the absolute value is greater than the current answer
+            # and its negation was seen before,
+            # update the answer
+            if abs_num > ans and -num + 1024 in seen:
                 ans = abs_num
-            seen.add(num)  # Insert the current number into the set
+            # Mark the current number as seen
+            seen.add(num + 1024)
 
         return ans
