bdc93023f674e7ff3766676c6ddd9de7fab2fc80
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-05-14 01:02:36 +0500

    Time: 44 ms (35.19%), Space: 16.6 MB (58.80%) - LeetHub

--- 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.py
@@ -2,23 +2,15 @@ class Solution:
     def matrixScore(self, grid: List[List[int]]) -> int:
         ROWS, COLS = len(grid), len(grid[0])
         
-        for r in range(ROWS):
-            if grid[r][0] == 0:
-                for c in range(COLS):
-                    grid[r][c] = 0 if grid[r][c] else 1
-                    
-        for c in range(COLS):
-            one_cnt = 0
-            for r in range(ROWS):
-                one_cnt += grid[r][c]
-            if one_cnt < ROWS - one_cnt:
-                for r in range(ROWS):
-                    grid[r][c] = 0 if grid[r][c] else 1
-                    
-        res = 0
-        for r in range(ROWS):
-            for c in range(COLS):
-                res += grid[r][c] << (COLS - c - 1)
+        res = ROWS * 2** (COLS - 1)
         
+        for c in range(1, COLS):
+            cnt = 0
+            for r in range(ROWS):
+                if grid[r][c] == grid[r][0]:
+                    cnt += 1
+            cnt = max(cnt, ROWS - cnt)
+            res += cnt * 2** (COLS - c - 1)
+            
         return res
                     
