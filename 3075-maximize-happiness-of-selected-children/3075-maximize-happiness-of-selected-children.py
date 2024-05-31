d2a810b658c9820d08047a6886b52a00c4f315e4
Jaweria Batool <118465170+Jaweria-B@users.noreply.github.com>
2024-05-31 23:16:58 +0500

    Time: 937 ms (6.53%), Space: 43.3 MB (69.23%) - LeetHub

---
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        # Convert the list into a max heap by inverting the happiness values 
+        # Python's default heap data structure is a min heap
+        max_heap = [-h for h in happiness]
+        heapq.heapify(max_heap)
+        
+        total_happiness_sum = 0
+        turns = 0
+
+        for i in range(k):
+            # Invert again to get the original value
+            total_happiness_sum += max(-heapq.heappop(max_heap) - turns, 0)
+
+            # Increment turns for the next iteration
+            turns += 1
+            
+        return total_happiness_sum
